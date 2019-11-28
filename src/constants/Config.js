4a76f0c949c3f5ee88676aa77ad27969b90f97cc
ThanhTuan <thanhtuan090398@gmail.com>
2019-11-28 17:05:35 +0700

    fix

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,4 +1,4 @@
-//export const API_URL = 'https://realestate-nodejs-tlcn.herokuapp.com';
-
-export const API_URL ='https://realstatebackend.herokuapp.com';
-export const GOOGLE_CLIENT_ID = '776676760586-oeh418goat5ve4ng1r7dt0c2l9s5ihhv.apps.googleusercontent.com';
+export const API_URL = 'https://realestate-nodejs-tlcn.herokuapp.com';
+//export const API_URL = 'https://realstatebackend.herokuapp.com';
+//export const GOOGLE_CLIENT_ID = '776676760586-oeh418goat5ve4ng1r7dt0c2l9s5ihhv.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = '944943247508-daitn18el3ksjl6ebct79mh4pds2s8q4.apps.googleusercontent.com';
